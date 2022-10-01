63a0f3555dc63b484ec6a8b115ab0537d8eadd08
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2022-10-01 18:40:14 -0500

    first commit

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 
-function NewTaskForm() {
+function NewTaskForm({onTaskFormSubmit, categories}) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState("Code");
+
+  const handleText = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleCat = (e) => {
+    setCategory(e.target.value);
+  };
   return (
-    <form className="new-task-form">
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        onTaskFormSubmit({ category, text });
+      }}
+      className="new-task-form">
       <label>
         Details
-        <input type="text" name="text" />
+        <input onChange={handleText} value={text} type="text" name="text" />
       </label>
       <label>
         Category
-        <select name="category">
+        <select onChange={handleCat} value={category} name="category">
           {/* render <option> elements for each category here */}
+          {categories.map((catText) => {
+            return (
+              <option 
+                key={catText} 
+                value={catText}
+                className={catText}
+              >
+                {catText}
+              </option>
+            );
+          })}
         </select>
       </label>
       <input type="submit" value="Add task" />
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Task from "./Task"
 
-function TaskList() {
+function TaskList({tasks, deleteTask}) {
+  const displayTask = tasks.map((task, index) => {
+    return (
+      <Task
+        key={index}
+        {...task}
+        onClickDelete={() => deleteTask(task)}
+      />
+    );
+  })
+  
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {displayTask}
     </div>
   );
 }
